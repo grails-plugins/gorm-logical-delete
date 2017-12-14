1c52a6e2ebb8c9fed1705790749fb77dd694270f
Jeff Scott Brown <brownj@objectcomputing.com>
2017-12-13 20:11:01 -0500

    Satisfy codenarc

--- src/main/groovy/hibernate/logical/delete/HibernateLogicalDeleteGrailsPlugin.groovy
+++ src/main/groovy/hibernate/logical/delete/HibernateLogicalDeleteGrailsPlugin.groovy
@@ -5,23 +5,23 @@ import grails.plugins.*
 class HibernateLogicalDeleteGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.3.2 > *"
+    def grailsVersion = '3.3.2 > *'
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-        "grails-app/views/error.gsp"
+        'grails-app/views/error.gsp'
     ]
 
     // TODO Fill in these fields
-    def title = "Hibernate Logical Delete" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def title = 'Hibernate Logical Delete' // Headline display name of the plugin
+    def author = 'Your name'
+    def authorEmail = ''
     def description = '''\
 Brief summary/description of the plugin.
 '''
     def profiles = ['web']
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/hibernate-logical-delete"
+    def documentation = 'http://grails.org/plugin/hibernate-logical-delete'
 
     // Extra (optional) plugin metadata
 
@@ -40,31 +40,4 @@ Brief summary/description of the plugin.
     // Online location of the plugin's browseable source code.
 //    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
 
-    Closure doWithSpring() { {->
-            // TODO Implement runtime spring config (optional)
-        }
-    }
-
-    void doWithDynamicMethods() {
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    void doWithApplicationContext() {
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    void onChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    void onConfigChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    void onShutdown(Map<String, Object> event) {
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
 }
