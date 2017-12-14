6a9977b687268859be1828c0460c292e1ba54331
benrhine <rhine.ben@gmail.com>
2017-12-14 12:21:48 -0500

    listener needs improvement but can determine if an GormEntity allows soft delete or not

--- src/main/groovy/hibernate/logical/delete/PreQueryListener.groovy
+++ src/main/groovy/hibernate/logical/delete/PreQueryListener.groovy
@@ -1,29 +1,20 @@
 package hibernate.logical.delete
 
+import groovy.transform.CompileStatic
 import org.grails.datastore.mapping.query.event.PreQueryEvent
 import org.springframework.context.ApplicationListener
 import org.springframework.context.annotation.Scope
-import org.springframework.stereotype.Component
-import javax.annotation.Resource
-
-@Component("hibernateLogicalDeletePreQueryListener")
 @Scope("singleton")
+@CompileStatic
 class PreQueryListener implements ApplicationListener<PreQueryEvent> {
-    //@Resource def logicalDeleteGormEntity
 
     @Override
     void onApplicationEvent(PreQueryEvent event) {
-        final gormEntity = event.query.entity.javaClass
 
+        if(event.query.entity.persistentPropertyNames.contains('deleted')) {
+            println "This entity implements logical delete"
 
-//        if(logicalDeleteGormEntity[gormEntity]) {
-//            if(!event.query.session.getSessionProperty(HibernateLogicalDeleteGormEntity.PHYSICAL_SESSION)) {
-//                event.query.eq(
-//                        logicalDeleteGormEntity[gormEntity].property,
-//                        !logicalDeleteGormEntity[gormEntity].deletedState
-//                )
-//            }
-//        }
+            event.query.eq('deleted', false )
+        }
     }
-
 }
