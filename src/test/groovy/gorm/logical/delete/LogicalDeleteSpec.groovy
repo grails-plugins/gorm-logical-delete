dfd74418812365cd4785cce92ff56e60c23809b2
benrhine <rhine.ben@gmail.com>
2017-12-15 11:35:12 -0500

    adding tests

--- src/test/groovy/gorm/logical/delete/LogicalDeleteSpec.groovy
+++ src/test/groovy/gorm/logical/delete/LogicalDeleteSpec.groovy
@@ -89,6 +89,60 @@ class LogicalDeleteSpec extends Specification implements DomainUnitTest<Person> 
         !p.deleted
 
     }
+
+    @Rollback
+    void 'test dynamic finder'() {
+        given: 'a new, un-deleted person has been added'
+        new Person(userName: "Fred").save(flush:true)
+
+        when: 'a search is performed on non-deleted people'
+        final List<Person> nonDeletedPersonList = Person.findAllByDeleted(false)
+        Person p1 = nonDeletedPersonList.first()
+
+        then:
+        p1.userName == 'Fred'
+        !p1.deleted
+
+        when: 'a search is performed on deleted people'
+        p1.delete()
+        final List<Person> deletedPersonList = Person.findAllByDeleted(true)
+        Person p2 = deletedPersonList.first()
+
+        then:
+        p2.userName == 'Fred'
+        p2.deleted
+
+    }
+
+    @Rollback
+    void 'test findAll'() {
+        given: 'two new, un-deleted person has been added'
+        new Person(userName: "Bill").save(flush:true)
+        new Person(userName: "Ted").save(flush:true)
+
+        when: 'a search is performed on non-deleted people'
+        final List<Person> nonDeletedPersonList = Person.findAll()
+
+        then:
+        nonDeletedPersonList.size() == 2
+
+        when: 'a search is performed on deleted people'
+        Person p1 = nonDeletedPersonList.first()
+        p1.delete()
+        final List<Person> someDeletedPersonList = Person.findAll()
+
+        then:
+        someDeletedPersonList.size() == 2
+
+        when: 'a search is performed on deleted people'
+        Person p2 = nonDeletedPersonList.get(1)
+        p2.delete()
+        final List<Person> deletedPersonList = Person.findAll()
+
+        then:
+        deletedPersonList.size() == 2
+
+    }
 }
 
 @Entity
