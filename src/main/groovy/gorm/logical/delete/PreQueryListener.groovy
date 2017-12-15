cdf61521f6e4da7888897d76b4cf456ec634c607
Jeff Scott Brown <brownj@objectcomputing.com>
2017-12-15 08:36:05 -0500

    Cleaning up the listener

--- src/main/groovy/gorm/logical/delete/PreQueryListener.groovy
+++ src/main/groovy/gorm/logical/delete/PreQueryListener.groovy
@@ -6,9 +6,7 @@ import org.grails.datastore.mapping.model.PersistentEntity
 import org.grails.datastore.mapping.query.Query
 import org.grails.datastore.mapping.query.event.PreQueryEvent
 import org.springframework.context.ApplicationListener
-import org.springframework.context.annotation.Scope
 
-@Scope('singleton')
 @Slf4j
 @CompileStatic
 class PreQueryListener implements ApplicationListener<PreQueryEvent> {
@@ -19,27 +17,9 @@ class PreQueryListener implements ApplicationListener<PreQueryEvent> {
             Query query = event.query
             PersistentEntity entity = query.entity
 
-            /*
-                if(entity instanceof LogicalDelete)
-                I think this would be a better if statement but instanceof does not seem to apply to traits? but
-                instanceof only returns User
-             */
-            if (entity.persistentPropertyNames.contains('deleted')) {
-                log.debug 'This entity implements logical delete'
+            if (LogicalDelete.isAssignableFrom(entity.javaClass)) {
+                log.debug "This entity [${entity.javaClass}] implements logical delete"
 
-                // Pseudo Code: dont understand how to get this value here as its does not seem to be available, only
-                // property name?
-                // if allowQueryOfDeletedItems is not true then apply additional filter value
-                // allowQueryOfDeletedItems is a property on LogicalDelete that can be overridden by domain classes to
-                // specify
-                // if we want to allow searching of all deleted properties
-
-                // Need to get value, if value is true
-                /*
-                if (!entity.persistentPropertyNames.contains('allowQueryOfDeletedItems')) {
-                    query.eq('deleted', false)
-                }
-                */
                 query.eq('deleted', false)
             }
         } catch (Exception e) {
