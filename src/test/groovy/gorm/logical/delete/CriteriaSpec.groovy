30dce791cc173b2306f69bb181de4682a93836b6
niravassar <nirav_p_assar@yahoo.com>
2017-12-18 11:16:46 -0600

    - criteria api

---
+++ src/test/groovy/gorm/logical/delete/CriteriaSpec.groovy
@@ -0,0 +1,51 @@
+package gorm.logical.delete
+
+import grails.gorm.DetachedCriteria
+import grails.gorm.transactions.Rollback
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+/**
+ * This test suite focuses on the behavior of criteria API in collaboration with the PreQuery Listener
+ */
+class CriteriaSpec extends Specification implements DomainUnitTest<Person> {
+
+    Closure doWithSpring() { { ->
+            queryListener PreQueryListener
+        }
+    }
+
+    /******************* test criteria ***********************************/
+
+    @Rollback
+    void 'test criteria - logical deleted items'() {
+        given:
+        Person.createUsers()
+
+        // where detachedCriteria Call
+        when:
+        assert Person.count() == 3
+        Person.findByUserName("Ben").delete()
+        Person.findByUserName("Nirav").delete()
+        def criteria = Person.createCriteria()
+        def results = criteria {
+            or {
+                eq("userName", "Ben")
+                eq("userName", "Nirav")
+            }
+        }
+
+        then: "we should not get anything bc they were deleted"
+        !results
+
+        when:
+        results = criteria {
+            eq("userName", "Jeff")
+        }
+
+        then:
+        results
+        results[0].userName == 'Jeff'
+    }
+
+}
